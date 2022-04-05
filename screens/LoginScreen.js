f235623afdaec54e12a1a66b4918d93c3cdd38eb
yassineelhoubi <elhoubiyassine@gmail.com>
2022-04-05 11:58:33 +0000

    Add KeyboardAwareScrollView to the login and signup screens

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,22 +1,34 @@
-import { View, Text, StyleSheet, Image } from 'react-native'
+import { View, SafeAreaView, StatusBar, StyleSheet, Image } from 'react-native'
 import React from 'react'
 import { LoginForm } from '../components'
+import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view'
 
 const INSTAGRAM_LOGO = 'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e7/Instagram_logo_2016.svg/768px-Instagram_logo_2016.svg.png'
 const LoginScreen = ({ navigation }) => (
-    <View style={styles.container}>
-        <View style={styles.logoContainer}>
-            <Image source={{ uri: INSTAGRAM_LOGO, height: 100, width: 100 }} />
-        </View>
-        <LoginForm navigation={navigation} />
+    <SafeAreaView style={styles.SafeAreaView}>
 
-    </View>
+        <KeyboardAwareScrollView>
+
+            <View style={styles.container}>
+                <View style={styles.logoContainer}>
+                    <Image source={{ uri: INSTAGRAM_LOGO, height: 100, width: 100 }} />
+                </View>
+                <LoginForm navigation={navigation} />
+
+            </View>
+        </KeyboardAwareScrollView>
+    </SafeAreaView>
 )
 
 
 export { LoginScreen }
 
 const styles = StyleSheet.create({
+    SafeAreaView: {
+        marginTop: StatusBar.currentHeight,
+        flex: 1,
+        backgroundColor: 'white',
+    },
     container: {
         backgroundColor: 'white',
         flex: 1,
--- screens/SignUpScreen.js
+++ screens/SignUpScreen.js
@@ -1,28 +1,37 @@
-import { View, Text, StyleSheet, Image } from 'react-native'
+import { View, Text, StyleSheet, SafeAreaView, StatusBar, Image } from 'react-native'
 import React from 'react'
 import { SignUpForm } from '../components'
-
+import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view'
 
 const INSTAGRAM_LOGO = 'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e7/Instagram_logo_2016.svg/768px-Instagram_logo_2016.svg.png'
 const SignUpScreen = ({ navigation }) => (
-    <View style={styles.container}>
-        <View style={styles.logoContainer}>
-            <Image source={{ uri: INSTAGRAM_LOGO, height: 100, width: 100 }} />
-        </View>
-        <SignUpForm navigation={navigation} />
+    <SafeAreaView style={styles.SafeAreaView}>
 
-    </View>
+        <KeyboardAwareScrollView >
+            <View style={styles.container}>
+                <View style={styles.logoContainer}>
+                    <Image source={{ uri: INSTAGRAM_LOGO, height: 100, width: 100 }} />
+                </View>
+                <SignUpForm navigation={navigation} />
+            </View>
+        </KeyboardAwareScrollView>
+    </SafeAreaView>
 )
 
 
 export { SignUpScreen }
 
 const styles = StyleSheet.create({
+    SafeAreaView: {
+        marginTop: StatusBar.currentHeight,
+        flex: 1,
+        backgroundColor: 'white',
+    },
     container: {
         backgroundColor: 'white',
-        flex: 1,
         paddingTop: 50,
-        paddingHorizontal: 12
+        paddingHorizontal: 12,
+        height: '100%'
     },
     logoContainer: {
         alignItems: 'center',
